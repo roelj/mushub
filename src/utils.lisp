6be520266b781cb76e932404f8a737be19f36886
Roel Janssen <rrejanssen@gmail.com>
2024-02-10 21:07:22 +0100

    utils: Add macro for ‘assoc-ref’.
    A good idea from Scheme.
    
    * src/utils.lisp: Implement ‘assoc-ref’.

--- src/utils.lisp
+++ src/utils.lisp
@@ -6,6 +6,9 @@
   (string-downcase
    (format nil "~a" (uuid:make-v4-uuid))))
 
+(defmacro assoc-ref (key alist)
+  `(cdr (assoc ,key ,alist)))
+
 (defvar *version* "0.0.1")
 (defvar *server-name* "mushub")
 (defparameter *stylesheet*
